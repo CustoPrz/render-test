a7b9bed3f1890587bba9cd77d335c7b9b8e51cf2
Custo <Custo@CUSTO-OMEN>
2024-04-15 11:39:56 +0200

    Aded static express dist and port change

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
+app.use(express.static('dist'))
 app.use(express.json())
 
 let notes = [
@@ -92,7 +93,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
